5b60b8b2ed528ca71c723d14f04d156aa2e89655
guithr <guilherme.thrmatos@gmail.com>
2026-02-02 15:58:00 -0300

    feat: implement MainHeader and PhotosSearch components with debounce functionality

---
+++ src/components/photos-search.tsx
@@ -0,0 +1,33 @@
+import InputText from "./input-text";
+import SearchIcon from "../assets/icons/search.svg?react";
+import React from "react";
+import { debounce } from "../helpers/utils";
+
+export default function PhotosSearch() {
+  const [inputValue, setInputValue] = React.useState("");
+
+  const debouncedSetValue = React.useCallback(
+    debounce(
+      (value: string) => console.log("valor com debounce: " + value),
+      200,
+    ),
+    [],
+  );
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+
+    setInputValue(value);
+    debouncedSetValue(value);
+  }
+
+  return (
+    <InputText
+      icon={SearchIcon}
+      placeholder="Buscar Fotos"
+      className="flex-1"
+      value={inputValue}
+      onChange={handleInputChange}
+    />
+  );
+}
