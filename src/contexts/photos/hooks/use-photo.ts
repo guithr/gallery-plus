7975bcf0127399b67f760c7f79647f2bf5d2021e
Guilherme Matos <guilherme.thrmatos@gmail.com>
2026-02-11 20:28:06 -0300

    feat(photos): add toast feedback with sonner

--- src/contexts/photos/hooks/use-photo.ts
+++ src/contexts/photos/hooks/use-photo.ts
@@ -2,6 +2,7 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { api, fetcher } from "../../../helpers/api";
 import type { Photo } from "../models/photo";
 import type { PhotoNewFormSchema } from "../schemas";
+import { toast } from "sonner";
 
 interface PhotoDetailResponse extends Photo {
   nextPhotoId?: string;
@@ -41,7 +42,10 @@ export default function UsePhoto(id?: string) {
       }
 
       queryClyent.invalidateQueries({ queryKey: ["photos"] });
+
+      toast.success("Foto criada com sucesso!");
     } catch (error) {
+      toast.error("Erro ao criar foto");
       throw error;
     }
   }
