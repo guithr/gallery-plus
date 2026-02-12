0525cd52e0ad6e8dfb920920ff89a37345d78a75
guithr <guilherme.thrmatos@gmail.com>
2026-02-12 17:00:48 -0300

    feat(albums): add usePhotoAlbums hook for managing album photos

--- src/components/input-checkbox.tsx
+++ src/components/input-checkbox.tsx
@@ -1,72 +1,85 @@
 import Icon from "./icon";
 import { tv, type VariantProps } from "tailwind-variants";
-import CheckIcon from "../assets/icons/check.svg?react"
+import CheckIcon from "../assets/icons/check.svg?react";
 
 export const InputCheckboxWrapperVariants = tv({
-    base: `
+  base: `
     inline-flex items-center justify-center relative group
-    `
-})
+    `,
+  variants: {
+    disabled: {
+      true: "pointer-events-none opacity-80",
+    },
+  },
+});
 
 export const InputCheckboxVariants = tv({
-    base: `
+  base: `
     flex items-center justify-center 
     appearance-none peer
     transition overflow-hidden
     cursor-pointer
     `,
-    variants: {
-        variant: {
-            default: `
+  variants: {
+    variant: {
+      default: `
             border-2 border-solid
             border-border-primary
             hover:border-border-active
             checked:border-accent-brand checked:bg-accent-brand
             group-hover:checked:border-accent-brand-light
             group-hover:checked:bg-accent-brand-light
-            `
-        },
-        size: {
-            sm: "size-3 rounded-sm",
-            md: "size-5 rounded-sm"
-        },
-        disabled: {
-            true: "pointer-events-none"
-        }
+            `,
     },
-    defaultVariants: {
-        variant: "default",
-        size: "md",
-        disabled: false
-    }
-})
+    size: {
+      sm: "size-3 rounded-sm",
+      md: "size-5 rounded-sm",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "md",
+  },
+});
 
 export const InputCheckboxIconVariants = tv({
-    base: `
+  base: `
         absolute top-1/2 -translate-y-1/2
         hidden peer-checked:block
         fill-white
         cursor-pointer
     `,
-    variants: {
-        size: {
-            sm: "size-3 left-px",
-            md: "size-4 left-0.5"
-        }
+  variants: {
+    size: {
+      sm: "size-3 left-px",
+      md: "size-4 left-0.5",
     },
-    defaultVariants: {
-        size: "md"
-    }
-})
+  },
+  defaultVariants: {
+    size: "md",
+  },
+});
 
-interface InputTextProps extends VariantProps<typeof InputCheckboxVariants>, Omit<React.ComponentProps<"input">, "size" | "disabled"> { }
+interface InputTextProps
+  extends
+    VariantProps<typeof InputCheckboxVariants>,
+    Omit<React.ComponentProps<"input">, "size"> {}
 
-
-export default function InputCheckbox({ variant, size, disabled, className, ...props }: InputTextProps) {
-    return (
-        <label className={InputCheckboxWrapperVariants({className})}>
-            <input type="checkbox" {...props} className={InputCheckboxVariants({ variant, size, disabled })} />
-            <Icon svg={CheckIcon} className={InputCheckboxIconVariants({ size })} />
-        </label>
-    )
+export default function InputCheckbox({
+  variant,
+  size,
+  disabled,
+  className,
+  ...props
+}: InputTextProps) {
+  return (
+    <label className={InputCheckboxWrapperVariants({ className, disabled })}>
+      <input
+        type="checkbox"
+        {...props}
+        className={InputCheckboxVariants({ variant, size })}
+      />
+      <Icon svg={CheckIcon} className={InputCheckboxIconVariants({ size })} />
+    </label>
+  );
 }
--- src/contexts/albums/components/albums-list-selectable.tsx
+++ src/contexts/albums/components/albums-list-selectable.tsx
@@ -1,7 +1,9 @@
+import React from "react";
 import Divider from "../../../components/divider";
 import InputCheckbox from "../../../components/input-checkbox";
 import Skeleton from "../../../components/skeleton";
 import Text from "../../../components/text";
+import usePhotoAlbums from "../../photos/hooks/use-photo-albums";
 import type { Photo } from "../../photos/models/photo";
 import type { Album } from "../models/album";
 
@@ -16,6 +18,9 @@ export default function AlbumsListSelectable({
   albums,
   photo,
 }: AlbumsListSelectableProps) {
+  const { managePhotoOnAlbum } = usePhotoAlbums();
+  const [isUpdatingPhoto, setIsUpdatingPhoto] = React.useTransition();
+
   function isChecked(albumId: string) {
     return photo?.albums?.some((album) => album.id === albumId);
   }
@@ -29,6 +34,10 @@ export default function AlbumsListSelectable({
     } else {
       albumsIds = [...photo.albums.map((album) => album.id), albumId];
     }
+
+    setIsUpdatingPhoto(async () => {
+      await managePhotoOnAlbum(photo.id, albumsIds);
+    });
   }
 
   return (
@@ -44,7 +53,8 @@ export default function AlbumsListSelectable({
 
               <InputCheckbox
                 defaultChecked={isChecked(album.id)}
-                onClick={() => handlePhotoOnAlbums(album.id)}
+                onChange={() => handlePhotoOnAlbums(album.id)}
+                disabled={isUpdatingPhoto}
               />
             </div>
             {index !== albums.length - 1 && <Divider className="mt-4" />}
